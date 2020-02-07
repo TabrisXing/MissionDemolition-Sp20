114ef45098fb4e242c047dde3864b7e14bd0aa22
TabrisXing <xingtianx@gmail.com>
2020-02-06 20:39:07 -0500

    add shoot function
    also fix the slingshot

--- Assets/Slingshot.cs
+++ Assets/Slingshot.cs
@@ -8,6 +8,8 @@ public class Slingshot : MonoBehaviour
 
     public GameObject prefabProjectile;
 
+    public float velocityMult = 8f;
+
     [Header("Set Dynamically")]
 
     public GameObject launchPoint;
@@ -15,6 +17,8 @@ public class Slingshot : MonoBehaviour
     public Vector3 launchPos;
     public GameObject projectile; 
     public bool aimingMode;
+
+    private Rigidbody projectileRigidbody;
 
     void Awake()
     {
@@ -43,7 +47,8 @@ public class Slingshot : MonoBehaviour
         // Start it at the launchPoint
         projectile.transform.position = launchPos;
         // Set it to isKinematic for now
-        projectile.GetComponent<Rigidbody>().isKinematic = true;
+        projectileRigidbody = projectile.GetComponent<Rigidbody>();
+        projectileRigidbody.isKinematic = true;
     }
 
     // Start is called before the first frame update
@@ -55,7 +60,27 @@ public class Slingshot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (!aimingMode) return;
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);        Vector3 mouseDelta = mousePos3D - launchPos;        float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+
+        if (mouseDelta.magnitude > maxMagnitude)
+        {
+            mouseDelta.Normalize();
+            mouseDelta *= maxMagnitude;
+        }
+
+        Vector3 projPos = launchPos + mouseDelta;
+        projectile.transform.position = projPos;
+
+        if (Input.GetMouseButtonUp(0))
+        {   
+            aimingMode = false;
+            projectileRigidbody.isKinematic = false;
+            projectileRigidbody.velocity = -mouseDelta * velocityMult;
+            projectile = null;
+        }
     }
 
 

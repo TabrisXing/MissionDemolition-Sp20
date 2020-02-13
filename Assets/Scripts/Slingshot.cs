1d500e22f38344583fc0c9b22c44ad6d96147305
TabrisXing <xingtianx@gmail.com>
2020-02-12 20:41:14 -0500

    Add follow camera

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -79,6 +79,8 @@ public class Slingshot : MonoBehaviour
             aimingMode = false;
             projectileRigidbody.isKinematic = false;
             projectileRigidbody.velocity = -mouseDelta * velocityMult;
+
+            FollowCam.POI = projectile;
             projectile = null;
         }
     }

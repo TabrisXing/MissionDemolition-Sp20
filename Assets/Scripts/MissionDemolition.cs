f860f59a51a1fa009347a5493f5f4641307b0200
TabrisXing <xingtianx@gmail.com>
2020-02-15 17:01:16 -0500

    change build

--- Assets/Scripts/MissionDemolition.cs
+++ Assets/Scripts/MissionDemolition.cs
@@ -36,6 +36,21 @@ public class MissionDemolition : MonoBehaviour
         StartLevel();
     }
 
+    void Update()
+    {
+        UpdateGUI();
+        // Check for level end
+        if ((mode == GameMode.playing) && Goal.goalMet)
+        {
+            // Change mode to stop checking for level end
+            mode = GameMode.levelEnd;
+            // Zoom out
+            SwitchView("Show Both");
+            // Start the next level in 2 seconds
+            Invoke("NextLevel", 2f);
+        }
+    }
+
     void StartLevel()
     {
         // Get rid of the old castle if one exists
@@ -54,7 +69,7 @@ public class MissionDemolition : MonoBehaviour
         castle.transform.position = castlePos;
         shotsTaken = 0;
         // Reset the camera
-        SwitchView("wShow Both");
+        SwitchView("Show Both");
         ProjectileLine.S.Clear();
         // Reset the goal
         Goal.goalMet = false;
@@ -70,20 +85,6 @@ public class MissionDemolition : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        UpdateGUI();
-        // Check for level end
-        if ((mode == GameMode.playing) && Goal.goalMet)
-        {
-            // Change mode to stop checking for level end
-            mode = GameMode.levelEnd;
-            // Zoom out
-            SwitchView("Show Both");
-            // Start the next level in 2 seconds
-            Invoke("NextLevel", 2f);
-        }
-    }
 
     void NextLevel()
     {
